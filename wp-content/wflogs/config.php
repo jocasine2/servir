5a0123625b37c13c92c5110eaab74ba26dd78112
Joaquim Campelo da Silva Neto <jcsnetcc@gmail.com>
2023-01-09 14:09:46 -0300

    🎉 Cria repositorio

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"N~,!z7QY9wt%`|`G@`-7>=~wsf~lzduf>Rh9S>Z1n:m4RG)lN*.$7)pA/G3?GYmR";s:7:"version";s:5:"1.0.5";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1464;}
